4b345f0a673029b2053c4cb5aad42ee2005fe707
Luis Mattoso <lmattoso84@gmail.com>
2022-06-14 22:50:59 +0100

    Ajustment on 2-opt operator

--- 2-opt algorithm/ant-2-opt.py
+++ 2-opt algorithm/ant-2-opt.py
@@ -5,8 +5,7 @@ import numpy as np
 # from numpy.random import choice as np_choice
 
 class TwoOpt(object):
-
-    def __init__(self, distances, n_iterations, improvement_threshold=0.001, alpha=1, beta=1):
+    def __init__(self, distances, n_ants, n_best, n_iterations, evaporation, improvement_threshold, alpha=1, beta=1):
         """
         Args:
             distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
@@ -19,65 +18,29 @@ class TwoOpt(object):
         Example:
             ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)
         """
-        self.distances = distances
+        self.distances  = distances
         self.pheromone = np.ones(self.distances.shape) / len(distances)
         self.all_inds = range(len(distances))
+        self.n_ants = n_ants
+        self.n_best = n_best
         self.n_iterations = n_iterations
-        self.improvement_threshold = improvement_threshold
         self.alpha = alpha
         self.beta = beta
+        self.decay = 1 - evaporation
+        self.improvement_threshold = improvement_threshold
 
     def run(self):
         shortest_path = None
-        all_time_shortest_path = ("placeholder", np.inf) # melhor elemento encontrado ate ao momento
+        all_time_shortest_path = ("placeholder", np.inf)  # melhor elemento encontrado ate ao momento
         for i in range(self.n_iterations):
-            path = self.gen_path_random(0)
-            shortest_path = self.two_opt(path)
+            all_paths = self.gen_all_paths_random()
+            self.spread_pheronome(all_paths, self.n_best, shortest_path=shortest_path)
+            shortest_path = min(all_paths, key=lambda x: x[1])
             print(i, shortest_path)
             if shortest_path[1] < all_time_shortest_path[1]:
                 all_time_shortest_path = shortest_path
+            self.pheromone *= self.decay
         return all_time_shortest_path
-
-    def two_opt(self, route):
-        improvement_factor = 1
-        best_distance = self.path_dist(route)
-
-        # Continua ate nao encontrar melhorias
-        while improvement_factor > self.improvement_threshold:
-            distance_to_beat = best_distance
-            for first in range(1, len(route) - 2):
-                for last in range(first + 1, len(route)):
-                    # Troca a posicao de duas cidades
-                    new_route = self.two_opt_swap(route, first, last)
-
-                    # Verifica a distancia da nova rota
-                    new_distance = self.path_dist(new_route)
-
-                    if new_distance < best_distance:
-                        route = new_route
-                        best_distance = new_distance
-
-            # Calcula o fator de melhoria
-            improvement_factor = 1 - best_distance / distance_to_beat
-        return (route, best_distance)
-
-    # Troca a posicao de dois elementos
-    def two_opt_swap(self, r, i, k):
-        temp_array = r.copy()
-
-        # Troca a posição das cidades
-        temp = temp_array[i]
-        temp_array[i] = temp_array[k]
-        temp_array[k] = temp
-
-        # Ajusta as rotas
-        path = []
-        size = len(r) - 1
-        for i in range(size):
-            path.append((temp_array[i][0], temp_array[i + 1][0]))
-        path.append((temp_array[size][0], temp_array[0][0]))
-
-        return path
 
     def spread_pheronome(self, all_paths, n_best, shortest_path):
         sorted_paths = sorted(all_paths, key=lambda x: x[1])
@@ -112,7 +75,7 @@ class TwoOpt(object):
             prev = move
             visited.add(move)
         path.append((prev, start))  # going back to where we started, add connection to the first node
-        return path
+        return self.two_opt(path)
 
     # determina probabilisticamente a proxima cidade a visitar
     def pick_city(self, pheromone, dist, visited):
@@ -127,13 +90,53 @@ class TwoOpt(object):
             0]  # [0] - devolve o indice do elemento, 1 - 1 elemento, p - probablidade por elemento
         return move
 
+    def two_opt(self, route):
+        improvement_factor = 1
+        best_distance = self.path_dist(route)
+
+        # Continua ate nao encontrar melhorias
+        while improvement_factor > self.improvement_threshold:
+            distance_to_beat = best_distance
+            for first in range(1, len(route) - 2):
+                for last in range(first + 1, len(route)):
+                    # Troca a posicao de duas cidades
+                    new_route = self.two_opt_swap(route, first, last)
+
+                    # Verifica a distancia da nova rota
+                    new_distance = self.path_dist(new_route)
+
+                    if new_distance < best_distance:
+                        route = new_route
+                        best_distance = new_distance
+
+            # Calcula o fator de melhoria
+            improvement_factor = 1 - best_distance / distance_to_beat
+        return route
+
+    # Troca a posicao de dois elementos
+    def two_opt_swap(self, r, i, k):
+        temp_array = r.copy()
+
+        # Troca a posição das cidades
+        temp = temp_array[i]
+        temp_array[i] = temp_array[k]
+        temp_array[k] = temp
+
+        # Ajusta as rotas
+        path = []
+        size = len(r) - 1
+        for i in range(size):
+            path.append((temp_array[i][0], temp_array[i + 1][0]))
+        path.append((temp_array[size][0], temp_array[0][0]))
+
+        return path
 
 distancias = np.genfromtxt("../distancias.txt", dtype='i', delimiter='\t')  # usecols=(1,2,3,4,5,6,7,8,9,10))
 cities = np.genfromtxt("../cidades.txt", dtype=None, delimiter='\n', encoding='utf-8')
 
 print(distancias)
 
-pop = TwoOpt(distancias, n_iterations=500, improvement_threshold=0.0000000000001)
+pop = TwoOpt(distancias,n_ants=100, n_best=10, n_iterations=100, evaporation=0.05, improvement_threshold=0.001)
 best = pop.run()
 print('Best in all the iterations:')
 print(best)
